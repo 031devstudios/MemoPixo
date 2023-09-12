32c6b112f1c0b405f7632735249024d5a19c0cf9
Josh Robb <031devstudios@gmail.com>
2023-09-12 21:29:36 +0200

    Changed background and borders for colour squares

--- app.py
+++ app.py
@@ -1,42 +1,53 @@
-# Example file showing a basic pygame "game loop"
-
 import pygame
-
 pygame.init()
-
-screen = pygame.display.set_mode((940,720))
+screen = pygame.display.set_mode((1024,768))
 pygame.display.set_caption("MemoPixo")
 clock = pygame.time.Clock()
 
+####################
+## GAME VARIABLES ##
+####################
+square_x_size = 200
+square_y_size = 200
+blue_square_x = 156
+blue_square_y = 50
+red_square_x = 668
+red_square_y = 50
+green_square_x = 156
+green_square_y = 325
+yellow_square_x = 668
+yellow_square_y = 325
+boarder_width = 5
+
+# draws the background
 background = pygame.image.load('graphics/background.png')
 
-blue_square = pygame.Surface((150, 150))
-blue_square.fill('Blue')
-
-red_square = pygame.Surface((150, 150))
-red_square.fill('Red')
-
-green_square = pygame.Surface((150, 150))
-green_square.fill('Green')
-
-yellow_square = pygame.Surface((150, 150))
-yellow_square.fill('Yellow')
+# draws the 4 coloured squares
+blue_square = pygame.Rect(blue_square_x, blue_square_y, square_x_size, square_y_size)
+red_square = pygame.Rect(red_square_x, red_square_y, square_x_size, square_y_size)
+green_square = pygame.Rect(green_square_x, green_square_y, square_x_size, square_y_size)
+yellow_square = pygame.Rect(yellow_square_x, yellow_square_y, square_x_size, square_y_size)
 
 running = True
 
+# main game loop
 while running:
       
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-
+    
     screen.blit(background,(0,0))
-    screen.blit(blue_square,(200,100))
-    screen.blit(red_square,(600,100))
-    screen.blit(green_square,(200,400))
-    screen.blit(yellow_square,(600,400))
-
+    pygame.draw.rect(screen, 'BLUE', blue_square)
+    pygame.draw.rect(screen, 'BLACK', (blue_square_x, blue_square_y, square_x_size, square_y_size), boarder_width)
+    pygame.draw.rect(screen, 'RED', red_square)
+    pygame.draw.rect(screen, 'BLACK', (red_square_x, red_square_y, square_x_size, square_y_size), boarder_width)
+    pygame.draw.rect(screen, 'GREEN', green_square)
+    pygame.draw.rect(screen, 'BLACK', (green_square_x, green_square_y, square_x_size, square_y_size), boarder_width)
+    pygame.draw.rect(screen, 'YELLOW', yellow_square)
+    pygame.draw.rect(screen, 'BLACK', (yellow_square_x, yellow_square_y, square_x_size, square_y_size), boarder_width)
+    
     pygame.display.update()
     clock.tick(60)  
 
